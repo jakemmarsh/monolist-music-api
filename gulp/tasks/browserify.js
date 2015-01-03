45a2f403b198e03719ec6dfc148144d7495bd4b8
Jake Marsh <jakemmarsh@gmail.com>
2015-01-03 15:39:13 -0500

    fix browserify task

--- gulp/tasks/browserify.js
+++ gulp/tasks/browserify.js
@@ -1,20 +1,55 @@
 'use strict';
 
-var gulp       = require('gulp');
-var browserify = require('gulp-browserify');
-var gulpif     = require('gulp-if');
-var uglify     = require('gulp-uglify');
-var rename     = require('gulp-rename');
+var gulp         = require('gulp');
+var gulpif       = require('gulp-if');
+var gutil        = require('gulp-util');
+var source       = require('vinyl-source-stream');
+var streamify    = require('gulp-streamify');
+var rename       = require('gulp-rename');
+var watchify     = require('watchify');
+var browserify   = require('browserify');
+var uglify       = require('gulp-uglify');
+var handleErrors = require('../util/handle-errors');
+
+// Based on: http://blog.avisi.nl/2014/04/25/how-to-keep-a-fast-build-with-browserify-and-reactjs/
+function buildScript(file, watch) {
+
+  var bundler = browserify({
+    entries: ['./marketing/js/' + file],
+    cache: {},
+    packageCache: {},
+    fullPaths: true
+  });
+
+  if ( watch ) {
+    bundler = watchify(bundler);
+    bundler.on('update', function() {
+      rebundle();
+      gutil.log('Rebundle...');
+    });
+  }
+
+  function rebundle() {
+    var stream = bundler.bundle();
+    return stream.on('error', handleErrors)
+    .pipe(source(file))
+    .pipe(gulpif(global.isProd, streamify(uglify({
+      compress: { drop_console: true }
+    }))))
+    .pipe(streamify(rename({
+      basename: 'main',
+      suffix: '.min'
+    })))
+    .pipe(gulp.dest('./build/js'));
+  }
+
+  return rebundle();
+
+}
 
 gulp.task('browserify', function() {
 
-  return gulp.src('./marketing/js/main.js')
-  .pipe(browserify({
-    insertGlobals: true,
-    debug: true
-  }))
-  .pipe(gulpif(global.isProd, uglify()))
-  .pipe(rename({suffix: '.min'}))
-  .pipe(gulp.dest('./build/js'));
+  // Only run watchify if NOT production
+  return buildScript('main.js', !global.isProd);
 
 });
--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -4,16 +4,7 @@ var gulp = require('gulp');
 
 gulp.task('watch', function() {
 
-  // Watch our scripts
-  gulp.watch('./marketing/js/**/*.js',[
-    'lint',
-    'browserify'
-  ]);
-  // Watch our styles
-  gulp.watch('./marketing/styles/**/*.scss', [
-    'styles'
-  ]);
-  // Watch our templates, helpers, and data files
+  gulp.watch('./marketing/styles/**/*.scss', ['styles']);
   gulp.watch(['./marketing/pages/**/*.hbs', './marketing/templates/**/*.hbs', './marketing/data/*.json', './marketing/helpers/*.js'], [
     'assemble'
   ]);
