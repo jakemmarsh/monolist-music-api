8ff79f5747d198d78a25393e9e6e0517ca704d7d
Jake Marsh <jakemmarsh@gmail.com>
2015-02-11 17:31:13 -0500

    fix headers so that access-control-allow-origin is always set to request origin

--- server.js
+++ server.js
@@ -57,7 +57,7 @@ if ( process.env.NODE_ENV === 'production' ) {
 // Add headers
 server.use(function (req, res, next) {
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Origin', req.headers.origin);
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   // Request headers you wish to allow
