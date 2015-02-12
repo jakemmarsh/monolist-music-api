1ac33c78192c963f333cedfd7be428dcc7353faa
Jake Marsh <jakemmarsh@gmail.com>
2015-02-12 14:46:13 -0500

    fix youtube search logic to use ytdl-core to verify that each result has the proper format available (audio/webm). Filter results that do not

--- api/routes/sources/youtube.js
+++ api/routes/sources/youtube.js
@@ -10,83 +10,56 @@ var config  = require('../../../config');
 
 /* ====================================================== */
 
-/*
- * Convert YouTube's duration format to seconds
- */
-function parseYouTubeDuration(duration) {
-    var a = duration.match(/\d+/g);
-
-    if ( duration.indexOf('M') >= 0 && duration.indexOf('H') === -1 && duration.indexOf('S') === -1 ) {
-      a = [0, a[0], 0];
-    }
-    if ( duration.indexOf('H') >= 0 && duration.indexOf('M') === -1 ) {
-      a = [a[0], 0, a[1]];
-    }
-    if ( duration.indexOf('H') >= 0 && duration.indexOf('M') === -1 && duration.indexOf('S') === -1 ) {
-      a = [a[0], 0, 0];
-    }
-
-    duration = 0;
-
-    if ( a.length === 3 ) {
-      duration = duration + parseInt(a[0]) * 3600;
-      duration = duration + parseInt(a[1]) * 60;
-      duration = duration + parseInt(a[2]);
-    }
-    if ( a.length === 2 ) {
-      duration = duration + parseInt(a[0]) * 60;
-      duration = duration + parseInt(a[1]);
-    }
-    if ( a.length === 1 ) {
-      duration = duration + parseInt(a[0]);
-    }
-
-    return duration;
-}
-
-/* ====================================================== */
-
 exports.search = function(query, limit) {
 
   var mainDeferred = when.defer();
 
-  var getVideoDuration = function(infoUrl) {
+  var getVideoInfo = function(videoId) {
     var deferred = when.defer();
-    var duration;
+    var requestUrl = 'http://youtube.com/watch?v=' + videoId;
+    var matches = [];
+    var webmRegex = new RegExp('audio/webm', 'i');
+    var returnInfo = null;
 
-    request(infoUrl, function(err, response, body) {
+    ytdl.getInfo(requestUrl, { downloadURL: true }, function(err, info) {
       if ( err ) {
         deferred.reject({ status: 500, body: err.toString() });
       } else {
-        body = JSON.parse(body);
+        if ( info.formats ) {
+          matches = _.filter(info.formats, function(format) {
+            return webmRegex.test(format.type);
+          });
 
-        duration = body.items ? parseYouTubeDuration(body.items[0].contentDetails.duration) : null;
-        deferred.resolve(duration);
+          if ( matches && matches.length ) {
+            returnInfo = {
+              duration: parseInt(info.length_seconds)
+            };
+          }
+        }
       }
+      deferred.resolve(returnInfo);
     });
 
     return deferred.promise;
   };
 
-  var addVideoDurations = function(videos) {
+  var filterAndAddVideoInfo = function(videos) {
     var deferred = when.defer();
-    var infoUrl = 'https://www.googleapis.com/youtube/v3/videos?';
-    var infoParameters = {
-      part: 'contentDetails',
-      key: config.youtube.key
-    };
     var promises = [];
 
     _.each(videos, function(videoObject) {
-      infoParameters.id = videoObject.sourceParam;
-
-      promises.push(getVideoDuration(infoUrl + qs.stringify(infoParameters)));
+      promises.push(getVideoInfo(videoObject.sourceParam));
     });
 
-    when.all(promises).then(function(durations) {
-      _.each(durations, function(duration, index) {
-        videos[index].duration = duration;
+    when.all(promises).then(function(videoInfos) {
+      _.each(videoInfos, function(info, index) {
+        if ( !info ) {
+          videos[index] = null;
+        } else {
+          videos[index].duration = info.duration;
+        }
       });
+      videos = _.reject(videos, function(video) { return _.isEmpty(video); });
       deferred.resolve(videos);
     }, function(err) {
       deferred.reject(err);
@@ -125,14 +98,16 @@ exports.search = function(query, limit) {
           };
         });
 
-        deferred.resolve(addVideoDurations(searchResults));
+        deferred.resolve(searchResults);
       }
     });
 
     return deferred.promise;
   };
 
-  getSearchResults(query).then(function(results) {
+  getSearchResults(query)
+  .then(filterAndAddVideoInfo)
+  .then(function(results) {
     mainDeferred.resolve(results);
   }, function(err) {
     mainDeferred.reject({ status: 500, body: err.toString() });
@@ -147,8 +122,6 @@ exports.search = function(query, limit) {
 exports.stream = function(req, res) {
 
   var attemptRequest = function(videoInfo, cb) {
-    console.log('trying:', videoInfo);
-
     request(videoInfo.url)
     .on('error', function(err) {
       cb(err);
