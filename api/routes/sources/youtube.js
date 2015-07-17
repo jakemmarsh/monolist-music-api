19b63c155c5c10e3bd9d1e713a616c054aea91fa
Jake Marsh <jakemmarsh@gmail.com>
2015-07-17 13:17:59 -0700

    simplify logic for adding duration to youtube search results

--- api/routes/sources/youtube.js
+++ api/routes/sources/youtube.js
@@ -9,59 +9,66 @@ var fallback = require('fallback');
 
 /* ====================================================== */
 
+function parseYouTubeDuration(duration) {
+  var a = duration.match(/\d+/g);
+
+  if ( duration.indexOf('M') >= 0 && duration.indexOf('H') === -1 && duration.indexOf('S') === -1 ) {
+    a = [0, a[0], 0];
+  }
+  if ( duration.indexOf('H') >= 0 && duration.indexOf('M') === -1 ) {
+    a = [a[0], 0, a[1]];
+  }
+  if ( duration.indexOf('H') >= 0 && duration.indexOf('M') === -1 && duration.indexOf('S') === -1 ) {
+    a = [a[0], 0, 0];
+  }
+
+  duration = 0;
+
+  if ( a.length === 3 ) {
+    duration = duration + parseInt(a[0]) * 3600;
+    duration = duration + parseInt(a[1]) * 60;
+    duration = duration + parseInt(a[2]);
+  }
+  if ( a.length === 2 ) {
+    duration = duration + parseInt(a[0]) * 60;
+    duration = duration + parseInt(a[1]);
+  }
+  if ( a.length === 1 ) {
+    duration = duration + parseInt(a[0]);
+  }
+
+  return duration;
+}
+
+/* ====================================================== */
+
 exports.search = function(query, limit) {
 
   var mainDeferred = when.defer();
 
-  var getVideoInfo = function(videoId) {
+  var addVideoDurations = function(videos) {
     var deferred = when.defer();
-    var requestUrl = 'http://youtube.com/watch?v=' + videoId;
-    var matches = [];
-    var webmRegex = new RegExp('audio/webm', 'i');
-    var returnInfo = null;
+    var infoUrl = 'https://www.googleapis.com/youtube/v3/videos?';
+    var infoParameters = {
+      part: 'contentDetails',
+      key: process.env.YOUTUBE_KEY,
+      id: _.pluck(videos, 'sourceParam').join(',')
+    };
 
-    ytdl.getInfo(requestUrl, { downloadURL: true }, function(err, info) {
+    infoUrl += qs.stringify(infoParameters);
+
+    request(infoUrl, function(err, response, body) {
       if ( err ) {
-        deferred.resolve(null);
+        deferred.reject({ status: 500, body: err.toString() });
       } else {
-        if ( info.formats ) {
-          matches = _.filter(info.formats, function(format) {
-            return webmRegex.test(format.type);
-          });
+        body = JSON.parse(body);
 
-          if ( matches && matches.length ) {
-            returnInfo = {
-              duration: parseInt(info.length_seconds)
-            };
-          }
-        }
+        _.each(_.pluck(body.items, 'contentDetails'), function(contentDetails, index) {
+          videos[index].duration = parseYouTubeDuration(contentDetails.duration);
+        });
+
+        deferred.resolve(videos);
       }
-      deferred.resolve(returnInfo);
-    });
-
-    return deferred.promise;
-  };
-
-  var filterAndAddVideoInfo = function(videos) {
-    var deferred = when.defer();
-    var promises = [];
-
-    _.each(videos, function(videoObject) {
-      promises.push(getVideoInfo(videoObject.sourceParam));
-    });
-
-    when.all(promises).then(function(videoInfos) {
-      _.each(videoInfos, function(info, index) {
-        if ( !info ) {
-          videos[index] = null;
-        } else {
-          videos[index].duration = info.duration;
-        }
-      });
-      videos = _.reject(videos, function(video) { return _.isEmpty(video); });
-      deferred.resolve(videos);
-    }, function(err) {
-      deferred.reject(err.body || err);
     });
 
     return deferred.promise;
@@ -105,7 +112,7 @@ exports.search = function(query, limit) {
   };
 
   getSearchResults(query)
-  .then(filterAndAddVideoInfo)
+  .then(addVideoDurations)
   .then(function(results) {
     mainDeferred.resolve(results);
   }, function(err) {
