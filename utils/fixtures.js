5d90c00ad0f8a21f8aea67fa26d4796afa138bea
Jake Marsh <jakemmarsh@gmail.com>
2015-07-19 19:28:52 -0700

    move test fixtures into separate file, relocate populateDb

---
+++ utils/fixtures.js
@@ -0,0 +1,108 @@
+'use strict';
+
+module.exports = {
+
+  users: [
+    {
+      username: 'jakemmarsh',
+      email: 'jakemmarsh@gmail.com',
+      facebookId: '621883172',
+      hash: 'pass',
+      role: 'admin'
+    },
+    {
+      username: 'test',
+      email: 'test@gmail.com',
+      hash: 'test'
+    },
+    {
+      username: 'testTwo',
+      email: 'testTwo@gmail.com',
+      hash: 'test'
+    },
+  ],
+
+  playlists: [
+    {
+      UserId: 1,
+      owner: 'jakemmarsh',
+      title: 'Test Playlist',
+      privacy: 'public',
+      tags: ['test', 'hip hop', 'rap'],
+      imageUrl: 'http://franthony.com/wp-content/uploads/2015/04/record-player.jpg'
+    },
+    {
+      UserId: 1,
+      owner: 'jakemmarsh',
+      title: 'Second Playlist That Is Private',
+      privacy: 'private'
+    }
+  ],
+
+  collaborations: [
+    {
+      PlaylistId: 1,
+      UserId: 2
+    }
+  ],
+
+  playlistLikes: [
+    {
+      UserId: 1,
+      PlaylistId: 1
+    },
+    {
+      UserId: 2,
+      PlaylistId: 1
+    },
+    {
+      UserId: 1,
+      PlaylistId: 2
+    }
+  ],
+
+  playlistPlays: [
+    {
+      UserId: 1,
+      PlaylistId: 1
+    },
+    {
+      UserId: 2,
+      PlaylistId: 1
+    },
+    {
+      UserId: 2,
+      PlaylistId: 2
+    }
+  ],
+
+  tracks: [
+    {
+      imageUrl: 'https://i1.sndcdn.com/artworks-000086001473-mw7dye-large.jpg',
+      PlaylistId: 1,
+      UserId: 1,
+      source: 'soundcloud',
+      sourceParam: '159945668',
+      sourceUrl: 'http://soundcloud.com/rustie/attak-feat-danny-brown',
+      title: 'Attak (feat. Danny Brown)',
+      duration: 181
+    }
+  ],
+
+  groups: [
+    {
+      OwnerId: 2,
+      title: 'Test Group',
+      description: 'This is a group for anyone since it is just for testing.',
+      imageUrl: 'https://scontent-sjc2-1.xx.fbcdn.net/hphotos-xap1/v/t1.0-9/10375152_10153451820467673_5915045047010730686_n.jpg?oh=3eec477b3d0925b8f39802bbb68c3789&oe=565AA6AE'
+    }
+  ],
+
+  groupMemberships: [
+    {
+      GroupId: 1,
+      UserId: 1
+    }
+  ]
+
+};
--- utils/populateDb.js
+++ utils/populateDb.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var when = require('when');
+var when     = require('when');
+var fixtures = require('./fixtures');
 
 /* ====================================================== */
 
@@ -8,15 +9,8 @@ module.exports = function(models, mailer) {
 
   var createUser = function() {
     var deferred = when.defer();
-    var user = {
-      username: 'jakemmarsh',
-      email: 'jakemmarsh@gmail.com',
-      facebookId: '621883172',
-      hash: 'pass',
-      role: 'admin'
-    };
 
-    models.User.create(user).then(function(createdUser) {
+    models.User.create(fixtures.users[0]).then(function(createdUser) {
       // mailer.sendWelcome(createdUser);
       deferred.resolve(createdUser);
     }).catch(function(err) {
@@ -28,13 +22,8 @@ module.exports = function(models, mailer) {
 
   var createSecondUser = function() {
     var deferred = when.defer();
-    var user = {
-      username: 'test',
-      email: 'test@gmail.com',
-      hash: 'test'
-    };
 
-    models.User.create(user).then(function(createdUser) {
+    models.User.create(fixtures.users[1]).then(function(createdUser) {
       deferred.resolve(createdUser);
     }).catch(function(err) {
       console.log('error creating second user:', err);
@@ -45,13 +34,8 @@ module.exports = function(models, mailer) {
 
   var createThirdUser = function() {
     var deferred = when.defer();
-    var user = {
-      username: 'testTwo',
-      email: 'testTwo@gmail.com',
-      hash: 'test'
-    };
 
-    models.User.create(user).then(function(createdUser) {
+    models.User.create(fixtures.users[2]).then(function(createdUser) {
       deferred.resolve(createdUser);
     }).catch(function(err) {
       console.log('error creating third user:', err);
@@ -62,16 +46,8 @@ module.exports = function(models, mailer) {
 
   var createPlaylist = function() {
     var deferred = when.defer();
-    var playlist = {
-      UserId: 1,
-      owner: 'jakemmarsh',
-      title: 'Test Playlist',
-      privacy: 'public',
-      tags: ['test', 'hip hop', 'rap'],
-      imageUrl: 'http://franthony.com/wp-content/uploads/2015/04/record-player.jpg'
-    };
 
-    models.Playlist.create(playlist).then(function(createdPlaylist) {
+    models.Playlist.create(fixtures.playlists[0]).then(function(createdPlaylist) {
       deferred.resolve(createdPlaylist);
     }).catch(function(err) {
       console.log('error creating playlists:', err);
@@ -82,14 +58,8 @@ module.exports = function(models, mailer) {
 
   var createSecondPlaylist = function() {
     var deferred = when.defer();
-    var playlist = {
-      UserId: 1,
-      owner: 'jakemmarsh',
-      title: 'Second Playlist That Is Private',
-      privacy: 'private'
-    };
 
-    models.Playlist.create(playlist).then(function(createdPlaylist) {
+    models.Playlist.create(fixtures.playlists[1]).then(function(createdPlaylist) {
       deferred.resolve(createdPlaylist);
     }).catch(function(err) {
       console.log('error creating playlists:', err);
@@ -100,12 +70,8 @@ module.exports = function(models, mailer) {
 
   var createCollaboration = function() {
     var deferred = when.defer();
-    var collaboration = {
-      PlaylistId: 1,
-      UserId: 2
-    };
 
-    models.Collaboration.create(collaboration).then(function(createdCollaboration) {
+    models.Collaboration.create(fixtures.collaborations[0]).then(function(createdCollaboration) {
       deferred.resolve(createdCollaboration);
     }).catch(function(err) {
       console.log('error creating collaboration:', err);
@@ -116,22 +82,8 @@ module.exports = function(models, mailer) {
 
   var createPlaylistLikes = function() {
     var deferred = when.defer();
-    var likes = [
-      {
-        UserId: 1,
-        PlaylistId: 1
-      },
-      {
-        UserId: 2,
-        PlaylistId: 1
-      },
-      {
-        UserId: 1,
-        PlaylistId: 2
-      }
-    ];
 
-    models.PlaylistLike.bulkCreate(likes, { hooks: false, individualHooks: true }).then(function(createdLikes) {
+    models.PlaylistLike.bulkCreate(fixtures.playlistLikes, { hooks: false, individualHooks: true }).then(function(createdLikes) {
       deferred.resolve(createdLikes);
     }).catch(function(err) {
       console.log('error creating likes:', err);
@@ -142,22 +94,8 @@ module.exports = function(models, mailer) {
 
   var createPlaylistPlays = function() {
     var deferred = when.defer();
-    var plays = [
-      {
-        UserId: 1,
-        PlaylistId: 1
-      },
-      {
-        UserId: 2,
-        PlaylistId: 1
-      },
-      {
-        UserId: 2,
-        PlaylistId: 2
-      }
-    ];
 
-    models.PlaylistPlay.bulkCreate(plays, { hooks: false, individualHooks: true }).then(function(createdPlays) {
+    models.PlaylistPlay.bulkCreate(fixtures.playlistPlays, { hooks: false, individualHooks: true }).then(function(createdPlays) {
       deferred.resolve(createdPlays);
     }).catch(function(err) {
       console.log('error creating plays:', err);
@@ -168,18 +106,8 @@ module.exports = function(models, mailer) {
 
   var addTrackToPlaylist = function() {
     var deferred = when.defer();
-    var track = {
-      imageUrl: 'https://i1.sndcdn.com/artworks-000086001473-mw7dye-large.jpg',
-      PlaylistId: 1,
-      UserId: 1,
-      source: 'soundcloud',
-      sourceParam: '159945668',
-      sourceUrl: 'http://soundcloud.com/rustie/attak-feat-danny-brown',
-      title: 'Attak (feat. Danny Brown)',
-      duration: 181
-    };
 
-    models.Track.create(track).then(function(createdTrack) {
+    models.Track.create(fixtures.tracks[0]).then(function(createdTrack) {
       deferred.resolve(createdTrack);
     }).catch(function(err) {
       deferred.reject('error creating track:', err);
@@ -188,13 +116,8 @@ module.exports = function(models, mailer) {
 
   var createGroup = function() {
     var deferred = when.defer();
-    var group = {
-      title: 'Test Group',
-      description: 'This is a group for anyone since it is just for testing.',
-      imageUrl: 'https://scontent-sjc2-1.xx.fbcdn.net/hphotos-xap1/v/t1.0-9/10375152_10153451820467673_5915045047010730686_n.jpg?oh=3eec477b3d0925b8f39802bbb68c3789&oe=565AA6AE'
-    };
 
-    models.Group.create(group).then(function(createdGroup) {
+    models.Group.create(fixtures.groups[0]).then(function(createdGroup) {
       deferred.resolve(createdGroup);
     }).catch(function(err) {
       console.log(err);
@@ -206,12 +129,8 @@ module.exports = function(models, mailer) {
 
   var addUserToGroup = function(group) {
     var deferred = when.defer();
-    var membership = {
-      GroupId: group.id,
-      UserId: 1
-    };
 
-    models.GroupMembership.create(membership).then(function(createdMembership) {
+    models.GroupMembership.create(fixtures.groupMemberships[0]).then(function(createdMembership) {
       deferred.resolve(createdMembership);
     }).catch(function(err) {
       deferred.reject('error creating membership:', err);
