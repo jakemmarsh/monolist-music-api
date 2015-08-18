6123eb09ec3d1500e6b676ae0ea8a292d43d3b8b
Jake Marsh <jake@wealthfront.com>
2015-08-18 11:42:45 -0700

    store post track as simple JSON

--- api/models/Post.js
+++ api/models/Post.js
@@ -3,14 +3,14 @@
 module.exports = function(sequelize, DataTypes) {
 
   var Post = sequelize.define('Post', {
-    body: { type: DataTypes.STRING, allowNull: false }
+    body:  { type: DataTypes.STRING, allowNull: false },
+    track: { type: DataTypes.JSON, allowNull: true }
   },
   {
     classMethods: {
       associate: function(models) {
         Post.belongsTo(models.User);
         Post.belongsTo(models.Group);
-        Post.hasOne(models.Track);
         Post.hasMany(models.PostDownvote, { as: 'Downvotes', onDelete: 'cascade' });
         Post.hasMany(models.PostUpvote, { as: 'Upvotes', onDelete: 'cascade' });
         Post.hasMany(models.PostComment, { as: 'Comments', onDelete: 'cascade' });
