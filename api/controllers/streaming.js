037b3dbe7a3addb3b63481ac9f69527784349076
Jake Marsh <jake@wealthfront.com>
2015-08-21 14:19:42 -0700

    code cleanup

--- api/controllers/streaming.js
+++ api/controllers/streaming.js
@@ -1,14 +1,6 @@
 'use strict';
 
-var path       = require('path');
-var bandcamp   = require(path.join(__dirname, 'sources/bandcamp'));
-var soundcloud = require(path.join(__dirname, 'sources/soundcloud'));
-var spotify    = require(path.join(__dirname, 'sources/spotify'));
-var youtube    = require(path.join(__dirname, 'sources/youtube'));
-
-/* ====================================================== */
-
-exports.bandcamp   = bandcamp.stream;
-exports.soundcloud = soundcloud.stream;
-exports.spotify    = spotify.stream;
-exports.youtube    = youtube.stream;
+exports.bandcamp   = require('./sources/bandcamp').stream;
+exports.soundcloud = require('./sources/soundcloud').stream;
+exports.spotify    = require('./sources/spotify').stream;
+exports.youtube    = require('./sources/youtube').stream;
