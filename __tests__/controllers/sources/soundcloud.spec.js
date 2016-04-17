cbf75c813ace4284dbadcd1f9c73e0107e443b1f
Jake Marsh <jakemmarsh@gmail.com>
2016-04-17 11:57:08 -0700

    fix soundcloud test

--- __tests__/controllers/sources/soundcloud.spec.js
+++ __tests__/controllers/sources/soundcloud.spec.js
@@ -19,7 +19,7 @@ describe('soundcloud routes', function() {
   });
 
   it('should get the details of a track from URL', function(done) {
-    var trackUrl = 'https://soundcloud.com/nickraymondg/skizzy-mars-weekend-millionaires-remix';
+    var trackUrl = 'https://soundcloud.com/dallascotton/something-new';
 
     request(url)
     .get('details/soundcloud/' + encodeURIComponent(trackUrl))
